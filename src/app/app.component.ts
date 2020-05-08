506aa35404b8196e51464595082e87346c80c162
ny.iuriii@gmail.com <ny.iuriii@gmail.com>
2020-05-08 18:36:17 +0200

    fist for this app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,65 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw';
+  password = '';
+  includeLetters = false;
+  includeNumber = false;
+  includeSymbols = false;
+  length = 0;
+
+  onButtonClick() {
+    const numbers = '1234567890';
+    const letters = 'qwertyuiopasdfghjklzxcvbnm'; 
+    const symbols = '!@#$%^&*()';
+ 
+
+    let validChars = '';
+    if (this.includeLetters) {validChars += letters};
+    if (this.includeNumber) {validChars += numbers};
+    if (this.includeSymbols) {validChars += symbols}
+
+    let generatedPassword = '';
+    for (let i =0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index]
+      
+      
+     
+    }
+   this.password = generatedPassword;
+
+    
+    
+    
+    
+  }
+  onInput(inputClap:string) {
+    const number = parseInt(inputClap)
+      if(!isNaN(number))  {
+        this.length = number
+      }
+  }
+  
+
+  onIncludeLetter () {
+
+    this.includeLetters = !this.includeLetters;
+
+    console.log (this.includeLetters)
+  }
+
+  onNumber () {
+
+    this.includeNumber = !this.includeNumber;
+
+    console.log (this.includeNumber)}
+  
+  onSymbol () {
+
+      this.includeSymbols = !this.includeSymbols;
+  
+      console.log (this.includeSymbols)}
+  
+
+    
 }
